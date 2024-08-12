be652f63cdeeff52a710056985114bba01f86470
olikiverjesus <149892232+olikiverjesus@users.noreply.github.com>
2024-08-12 18:33:06 -0300

    Changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Add interactivity here
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            const targetId = e.target.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetSection.offsetTop - 70,
+                behavior: 'smooth'
+            });
+        });
+    });
 });
